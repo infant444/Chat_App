5be57bc9aa7cd2c81174297ee5dc92661365b2f7
infant04 <infant0467@gmail.com>
2023-06-28 12:10:27 +0530

    base

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { BlankComponent } from './Component/blank/blank.component';
+const routes: Routes = [
+  {path:'',component:BlankComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './Component/header/header.component';
+import { BlankComponent } from './Component/blank/blank.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { SearchComponent } from './Component/search/search.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BlankComponent,
+    ContactComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
